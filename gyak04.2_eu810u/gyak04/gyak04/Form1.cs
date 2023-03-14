b22cc4006c2e72930e473b046779527b9cc62a54
eu810u <eu810u@SZA0025889>
2023-03-14 09:02:32 +0100

    initial commit

--- gyak04.2_eu810u/gyak04/gyak04/Form1.cs
+++ gyak04.2_eu810u/gyak04/gyak04/Form1.cs
@@ -16,17 +16,51 @@ namespace gyak04
     {
         List<Flat> flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         void LoadData()
         {
             flats = context.Flats.ToList();
         
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+
+
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+
+            }
         }
     }
 }
