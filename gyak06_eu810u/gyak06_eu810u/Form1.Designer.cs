e13ae21585af56e0f2b0c49ba1f0b4e438550e83
eu810u <eu810u@SZA0025889>
2023-03-28 09:08:24 +0200

    :)

--- gyak06_eu810u/gyak06_eu810u/Form1.Designer.cs
+++ gyak06_eu810u/gyak06_eu810u/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace gyak06_eu810u
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mnbdataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.mnbdataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnbdataGridView
+            // 
+            this.mnbdataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mnbdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mnbdataGridView.Location = new System.Drawing.Point(12, 12);
+            this.mnbdataGridView.Name = "mnbdataGridView";
+            this.mnbdataGridView.Size = new System.Drawing.Size(344, 345);
+            this.mnbdataGridView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(363, 362);
+            this.Controls.Add(this.mnbdataGridView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mnbdataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView mnbdataGridView;
     }
 }
 
--- gyak06_eu810u/gyak06_eu810u/Form1.cs
+++ gyak06_eu810u/gyak06_eu810u/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using gyak06_eu810u.Entities;
+using gyak06_eu810u.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace gyak06_eu810u
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            var r = GetExchangeRates();
+            GetXmlData(r);
+            mnbdataGridView.DataSource = Rates;
+        }
+
+        private string GetExchangeRates()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
+            return result;
+
+        }
+
+
+        private void GetXmlData(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
         }
     }
 }
