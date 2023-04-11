30c28fc9d50b9c2fd24832023668961f63eeb9a2
eu810u <eu810u@SZA0025889>
2023-04-11 08:44:20 +0200

    :)

---
+++ gyak07_eu810u/gyak07_eu810u/Entities/BallFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak07_eu810u.Entities
+{
+    class BallFactory
+    {
+
+        public Ball CreateNew()
+        {
+            return new Ball();
+        
+        }
+    }
+}
--- gyak07_eu810u/gyak07_eu810u/Form1.cs
+++ gyak07_eu810u/gyak07_eu810u/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak07_eu810u.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,32 @@ namespace gyak07_eu810u
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
